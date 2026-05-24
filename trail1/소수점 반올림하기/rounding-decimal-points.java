c876b97284d071b6660c0e1849dd28339f3879a2
임민지 <126225434+im-minji@users.noreply.github.com>
2026-05-24 17:31:23 +0900

    [Passed] 소수점 반올림하기 (99ms, 9MB)

---
+++ trail1/소수점 반올림하기/rounding-decimal-points.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        double a = 25.352; 
+
+        double num = Math.round(a);
+        String n = String.format("%.1f", a); 
+
+        System.out.println(n); 
+    }
+}
