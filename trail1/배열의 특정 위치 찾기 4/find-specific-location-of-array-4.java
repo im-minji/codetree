ff1802c33034e0971f196871748efe1669352ea4
임민지 <126225434+im-minji@users.noreply.github.com>
2026-06-01 14:42:54 +0900

    [Passed] 배열의 특정 위치 찾기 4 (115ms, 21MB)

---
+++ trail1/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*; 
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine()); 
+        
+        int count = 0; 
+        int sum = 0;
+
+        for(int i=0; i<10; i++) {
+            int cur = Integer.parseInt(st.nextToken()); 
+            
+            if(cur == 0) {
+                break; 
+            }
+
+            if(cur%2==0) {
+                count++; 
+                sum += cur; 
+            }
+        }
+
+
+        bw.write(count + " ");
+        bw.write(sum + " "); 
+
+        bw.flush();
+        bw.close(); 
+    }
+}
